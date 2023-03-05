767d1b2c0ce5ecfc7fe602f4f9372d38441ef4a6
Gomuang <97213554+NewfileDOTpy@users.noreply.github.com>
2023-03-05 17:25:26 +0900

    2023/03/05
    디버그

--- gpp/gpp/src/main/java/com/gamepublishingproject/gpp/response/MultiResponseDto.java
+++ gpp/gpp/src/main/java/com/gamepublishingproject/gpp/response/MultiResponseDto.java
@@ -1,8 +1,8 @@
-package com.gamepublishingproject.gpp.category.response;
+package com.gamepublishingproject.gpp.response;
+
 
 import lombok.Getter;
 import org.springframework.data.domain.Page;
-
 import java.util.List;
 
 @Getter
@@ -12,7 +12,7 @@ public class MultiResponseDto<T> {
 
     public MultiResponseDto(List<T> data, Page page) {
         this.data = data;
-        this.pageInfo = new PageInfo(page.getNumber()+1,
+        this.pageInfo = new PageInfo(page.getNumber() + 1,
                 page.getSize(), page.getTotalElements(), page.getTotalPages());
     }
 }
